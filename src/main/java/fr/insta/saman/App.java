162c24a74b23470e25d5e4a4c4f178b7c0b987d1
saman1122 <saman11@hotmail.fr>
2018-03-08 14:33:00 +0100

    add 2 methods to get url matrix and normalised matrix

--- src/main/java/fr/insta/saman/App.java
+++ src/main/java/fr/insta/saman/App.java
@@ -5,12 +5,11 @@ import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Vector;
+import java.util.*;
 
 class App
 {
+    private static HashMap<String,Integer> listUniqueDomain = new HashMap<>();
 
     public static void main( String[] args ) throws URISyntaxException {
         List<Domain> listDomain = new ArrayList<>();
@@ -21,6 +20,7 @@ class App
 
         for (Node nc : listNodes) {
             listDomain.add(new Domain(nc.domain, nc.url, getLinksFromURL(nc.url)));
+            listUniqueDomain.putIfAbsent(nc.domain,listUniqueDomain.size()+1);
         }
 
         listDomain.forEach(x -> x.node.forEach(y ->  {
@@ -28,27 +28,49 @@ class App
 
         }) );
 
+        System.out.println(listUniqueDomain.toString());
+
+        Double [][] listTest = getMatriceUrl(listDomain);
+
+        for (int i = 0; i<listTest.length;i++) {
+            System.out.println();
+            for (int j = 0; j < listTest[i].length;j++) {
+                System.out.print( listTest[i][j] + "|");
+            }
+        }
+
+        System.out.println('\n');
+
+        Double [][] listTest2 = getMatriceNormalised(listTest);
+
+        for (int i = 0; i<listTest2.length;i++) {
+            System.out.println();
+            for (int j = 0; j < listTest2[i].length;j++) {
+                System.out.print( listTest2[i][j] + "|");
+            }
+        }
+
     }
 
     private static List<Node> getLinksFromURL(String url) throws URISyntaxException {
         List<Node> retour = new ArrayList<>();
         StringBuilder html = new StringBuilder();
 
-            try {
-                URL oracle = new URL(url);
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(oracle.openStream()));
+        try {
+            URL oracle = new URL(url);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(oracle.openStream()));
 
-                String inputLine;
-                while ((inputLine = in.readLine()) != null)
-                    html.append(inputLine);
-                in.close();
-            }catch (Exception ignored) {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                html.append(inputLine);
+            in.close();
+        }catch (Exception ignored) {
 
-            }
+        }
 
-            HTMLLinkExtractor htmlLinkExtractor = new HTMLLinkExtractor();
-            Vector<HTMLLinkExtractor.HtmlLink> links = htmlLinkExtractor.grabHTMLLinks(html.toString());
+        HTMLLinkExtractor htmlLinkExtractor = new HTMLLinkExtractor();
+        Vector<HTMLLinkExtractor.HtmlLink> links = htmlLinkExtractor.grabHTMLLinks(html.toString());
 
 
         for (HTMLLinkExtractor.HtmlLink htmlLink : links) {
@@ -57,6 +79,7 @@ class App
             nc.domain = getDomainName(nc.url);
             if (nc.domain.isEmpty()) nc.domain = getDomainName(url);
             if (!nc.url.startsWith("#")) retour.add(nc);
+            listUniqueDomain.putIfAbsent(nc.domain,listUniqueDomain.size()+1);
         }
 
         return retour;
@@ -71,7 +94,7 @@ class App
             String[] split = domain.split("\\.");
 
             if (split.length > 1)
-            domain = split[split.length-2] + "." + split[split.length-1];
+                domain = split[split.length-2] + "." + split[split.length-1];
         }
 
         return domain;
@@ -91,5 +114,43 @@ class App
         catch (Exception e) {
             return false;
         }
+    }
+
+    private static Double[][] getMatriceUrl( List<Domain> listDomain) {
+        Double retour[][] = new Double[listUniqueDomain.size()][listUniqueDomain.size()];
+
+        for (int i = 0; i<retour.length;i++) {
+            for (int j = 0; j<retour[i].length; j++) retour[i][j] = 0d;
+        }
+
+        listDomain.forEach(x -> x.node.forEach( y -> {
+            int domainColonne = listUniqueDomain.get(x.domain);
+            int domainLigne = listUniqueDomain.get(y.domain);
+
+            if (domainColonne != domainLigne) {
+                retour[domainLigne-1][domainColonne-1] += 1d;
+            }
+        }));
+
+        return retour;
+    }
+
+    private static Double[][] getMatriceNormalised(Double[][] matriceUrl) {
+        Double retour[][] = matriceUrl.clone();
+
+        for (int i=0; i<matriceUrl.length;i++) {
+            double total = 0;
+            for (int j = 0; j < matriceUrl[i].length; j++) {
+                total += matriceUrl[j][i];
+            }
+
+            if (total > 0d) {
+                for (int j = 0; j < matriceUrl[i].length; j++) {
+                    matriceUrl[j][i] /= total;
+                }
+            }
+        }
+
+        return retour;
     }
 }
